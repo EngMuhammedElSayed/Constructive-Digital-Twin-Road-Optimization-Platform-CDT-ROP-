d8971789a85b4431bcbccfd618b6fe790f921d34
EngMuhammedElSayed <muhammedgalal91@gmail.com>
2026-07-12 17:39:45 +0300

    Update cost.py

--- src/models/cost.py
+++ src/models/cost.py
@@ -1,3 +1,11 @@
-"""
-Construction Cost Model.
-"""
+from dataclasses import dataclass
+
+@dataclass
+class CostParameters:
+    """
+    Stores construction cost parameters.
+    """
+
+    pavement_cost: float
+    row_cost: float
+    earthwork_cost: float
