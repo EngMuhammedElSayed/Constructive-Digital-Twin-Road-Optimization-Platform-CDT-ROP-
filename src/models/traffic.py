1bad7ec926879d3bc29194a1c89e995d01031fde
EngMuhammedElSayed <muhammedgalal91@gmail.com>
2026-07-12 17:39:29 +0300

    Update traffic.py

--- src/models/traffic.py
+++ src/models/traffic.py
@@ -1,3 +1,12 @@
-"""
-Traffic Model.
-"""
+from dataclasses import dataclass
+
+@dataclass
+class TrafficData:
+    """
+    Stores traffic characteristics.
+    """
+
+    demand: float
+    capacity: float
+    peak_hour_factor: float
+    heavy_vehicle_percentage: float
